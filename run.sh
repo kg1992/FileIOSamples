cee91ba530226c4a52230c1f565f7255bee81753
kg1992 <kyunggeun1992@gmail.com>
2021-07-25 21:44:03 +0900

    run.sh : run.bat과 완전히 같은 일을 하도록 수정함

--- run.sh
+++ run.sh
@@ -1,16 +1,55 @@
 # file: run.sh
 # author: kyunggeun lee
-# brief: build FileIOSamples project
-if [$1 = ""]
-then
-    echo usage:
-    echo run [executable_name]
-    goto :eof
-else
-    pushd ./linux
-    ./$1
-    popd
-fi
+# brief: run target executables
+pushd linux
 
+echo
+echo "echo Hello Pipe Print | PipePrint"
+sleep 1
+echo Hello Pipe Print | ./PipePrint
+echo
+sleep 1
 
-    
+echo "ReadPrint Text.txt"
+sleep 1
+./ReadPrint Text.txt
+echo
+sleep 1
+
+echo "ReadBinaryPrintHex rgbymcbw.bmp"
+sleep 1
+./ReadBinaryPrintHex rgbymcbw.bmp
+echo
+sleep 1
+
+echo "CopyFile Text.txt TextCopy.txt"
+sleep 1
+./CopyFile Text.txt TextCopy.txt
+echo
+sleep 1
+
+echo "FileSize rgbymcbw.bmp"
+sleep 1
+./FileSize rgbymcbw.bmp
+echo
+sleep 1
+
+echo "FileEqual Text.txt TextCopy.txt"
+sleep 1
+./FileEqual Text.txt TextCopy.txt
+echo
+sleep 1
+
+echo "FileEqual Text.txt rgbymcbw.bmp"
+sleep 1
+./FileEqual Text.txt rgbymcbw.bmp
+echo
+sleep 1
+
+echo "FileEqual Text.txt TextSimilar.txt"
+sleep 1
+./FileEqual Text.txt TextSimilar.txt
+echo
+sleep 1
+
+popd
